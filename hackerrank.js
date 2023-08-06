ec5f352a90307edfb75a641506bf8becf52d956e
Jack Mabie <johnmabie94@gmail.com>
2023-08-06 13:57:53 -0500

    another one

--- hackerrank.js
+++ hackerrank.js
@@ -29,9 +29,11 @@ function plusMinus(plusMinusArr) {
 ${negRatio}
 ${zeroRatio}`
 
+    process.stdout.write("\n");
     process.stdout.write("plusMinus:")
     process.stdout.write("\n");
     process.stdout.write(res)
+    process.stdout.write("\n");
     // console.log("plusMinus Results:", res);
 }
 
@@ -39,7 +41,6 @@ plusMinus(plusMinusArr);
 
 /* stdout linebreak function */
 function lineBreak() {
-    process.stdout.write("\n");
     process.stdout.write("\n");
 }
 
@@ -60,6 +61,7 @@ function miniMaxSum(miniMaxArr) {
 
     process.stdout.write("miniMaxSum:")
     process.stdout.write(res);
+    process.stdout.write("\n");
 }
 
 miniMaxSum(miniMaxArr);
@@ -93,6 +95,7 @@ function timeConversion(timeString) {
 
     let convertedTime = `${convertedHour}:${minSec}`
 
+    process.stdout.write("\n");
     process.stdout.write("Time Given:");
     process.stdout.write("\n");
     process.stdout.write(timeString);
@@ -241,3 +244,21 @@ lineBreak();
 
 /* Week 1 - Problem 8: Counting Sort 1 */
 
+const countSortula = [
+    63, 25, 73, 1, 98, 73, 56, 84, 86, 57, 16, 83, 8, 25, 81, 56, 9, 53, 98, 67, 99, 12, 83, 89, 80, 91, 39, 86, 76, 85, 74, 39, 25, 90, 59, 10, 94, 32, 44, 3, 89, 30, 27, 79, 46, 96, 27, 32, 18, 21, 92, 69, 81, 40, 40, 34, 68, 78, 24, 87, 42, 69, 23, 41, 78, 22, 6, 90, 99, 89, 50, 30, 20, 1, 43, 3, 70, 95, 33, 46, 44, 9, 69, 48, 33, 60, 65, 16, 82, 67, 61, 32, 21, 79, 75, 75, 13, 87, 70, 33
+]
+
+function countingSort(countSortula) {
+    let countArr = new Array(100).fill(0);
+
+    for (let i of countSortula) {
+        countArr[i] += 1;
+    }
+
+    console.log("Sorted Count:", countArr);
+    return countArr;
+}
+
+countingSort(countSortula);
+
+lineBreak();
